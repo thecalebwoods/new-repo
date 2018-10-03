cea32488cd9f89ece80189c5119a9f84cbfe029c
thecalebwoods <thecalebwoods@gmail.com>
2018-10-03 10:27:18 -0400

    hello

--- projects/spark/fyb.js
+++ projects/spark/fyb.js
@@ -5,56 +5,57 @@ let cityText;
 let stateText;
 let nameText;
 
+let getInfo = function() {
+document.getElementById('contianer').innerText = "";
+cityText = document.getElementById('cityBrew').value;
+stateText = document.getElementById('stateBrew').value;
+nameText = document.getElementById('nameBrew').value;
+}
+
 let getUrl = function(){
-    document.getElementById('contianer').innerText = "";
-    cityText = document.getElementById('cityBrew').value;
-    stateText = document.getElementById('stateBrew').value;
-    nameText = document.getElementById('nameBrew').value;
+    getInfo();
     apiBrew = "https://api.openbrewerydb.org/breweries?by_name=" + nameText + "&by_city=" + cityText + "&by_state=" + stateText + "&page=1&per_page=30";
     getBrewAsync();
 }
 let apiPage2 = function(){
-    document.getElementById('contianer').innerText = "";
-    cityText = document.getElementById('cityBrew').value;
-    stateText = document.getElementById('stateBrew').value;
+    getInfo();
     apiBrew = "https://api.openbrewerydb.org/breweries?by_name=" + nameText + "&by_city=" + cityText + "&by_state=" + stateText + "&page=2&per_page=30";
     getBrewAsync();
 }
 let apiPage3 = function(){
-    document.getElementById('contianer').innerText = "";
-    cityText = document.getElementById('cityBrew').value;
-    stateText = document.getElementById('stateBrew').value;
+    getInfo();
     apiBrew = "https://api.openbrewerydb.org/breweries?by_name=" + nameText + "&by_city=" + cityText + "&by_state=" + stateText + "&page=3&per_page=30";
     getBrewAsync();
 }
 let apiPage4 = function(){
-    document.getElementById('contianer').innerText = "";
-    cityText = document.getElementById('cityBrew').value;
-    stateText = document.getElementById('stateBrew').value;
+    getInfo();
     apiBrew = "https://api.openbrewerydb.org/breweries?by_name=" + nameText + "&by_city=" + cityText + "&by_state=" + stateText + "&page=4&per_page=30";
     getBrewAsync();
 }
 let apiPage5 = function(){
-    document.getElementById('contianer').innerText = "";
-    cityText = document.getElementById('cityBrew').value;
-    stateText = document.getElementById('stateBrew').value;
+    getInfo();
     apiBrew = "https://api.openbrewerydb.org/breweries?by_name=" + nameText + "&by_city=" + cityText + "&by_state=" + stateText + "&page=5&per_page=30";
     getBrewAsync();
 }
-
+//Function that graps info from my api
 let getBrewAsync = async function(){
     try {
+    // Uses the exact url that was given to fetch the info 
     const response = await fetch(apiBrew)
+    // Grab the array of objects and assign them to the breweryArr var
     breweryArr = await response.json();
-	console.log(breweryArr);
-	updateContent();
+    console.log(breweryArr);
+    // Call the updateContent function
+    updateContent();
+    // If 'try' wasnt successful execute 'catch'
     } catch (err) {
     console.log('Something went wrong. :(');
     console.log(err);    
     }
 }
 
-
+// This function creates the elements in html that contains the info
+// that 
 let updateContent = function(){
 
     for(let i = 0; i < breweryArr.length; i++){
@@ -109,4 +110,5 @@ let updateContent = function(){
 
         setTimeout(() => barCon.classList.remove('hidden'), 200*i);
     }
+
 }
